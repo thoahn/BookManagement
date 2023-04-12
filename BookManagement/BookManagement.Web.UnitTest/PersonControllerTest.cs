4e35a601692bfd0f9d56c7a9de392aa7e3dc9234
thoahn <dogandagbasti@gmail.com>
2023-04-12 11:06:52 +0300

    unit test project added, port configurations updated

---
+++ BookManagement/BookManagement.Web.UnitTest/PersonControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using BookManagement.Web.Controllers;
+using BookManagement.Web.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace BookManagement.Web.Test
+{
+    public class PersonControllerTest
+    {
+        private readonly Mock<IPersonService> _mockRepo;
+
+        private readonly PersonController _controller;
+
+        public PersonControllerTest()
+        {
+            _mockRepo = new Mock<IPersonService>();
+            _controller = new PersonController(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void Index_ActionExecute_ReturnView()
+        {
+            var result = _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
